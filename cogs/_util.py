63044d45903504774ec3364897800bab538ba478
developomp <developomp@gmail.com>
2021-02-07 21:13:26 +0900

    added NoAdminChannel exception

--- cogs/_util.py
+++ cogs/_util.py
@@ -5,6 +5,10 @@ import discord
 from discord.ext import commands
 
 
+class NotAdminChannel(discord.ext.commands.errors.CheckFailure):
+	pass
+
+
 def must_be_admin():
 	"""Discord bot command decorator.
 	Put it under @discord.ext.commands.command()
@@ -26,7 +30,9 @@ def lists_has_intersection(list1: list, list2: list) -> bool:
 
 
 def url_from_str(string: str) -> list:
-	# https://daringfireball.net/2010/07/improved_regex_for_matching_urls
+	"""Extract urls from string
+	https://daringfireball.net/2010/07/improved_regex_for_matching_urls
+	"""
 	url = re.findall(
 		r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))",
 		string
