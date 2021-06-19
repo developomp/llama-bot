b5c7a2e7584509d99444c308baf07e899413df74
developomp <developomp@gmail.com>
2021-06-19 11:16:52 +0900

    fixed messy help command

--- bot/cogs/core.py
+++ bot/cogs/core.py
@@ -66,22 +66,21 @@ Shows info about `ping` command:
 > {prefix}{command} ping""",
     )
     async def help(self, ctx, cog_str=None):
-        cogs = list(self.bot.cogs.keys())
-
-        if not cog_str:
+        if not cog_str:  # when no argument is passed
             help_embed = discord.Embed(
                 title="Help",
-                description=f"Use `{self.bot.command_prefix}help <cog>` command to get more information on a cog. (not case sensitive)",
+                description=f"Use `{self.bot.command_prefix}help <cog>` command to get more information on a cog. (case insensitive)",
             )
 
-            # Show command to get help for a cog
-            for cog in cogs:
+            for cog_name in self.bot.cogs:
+                # Show command to get help for a cog
                 help_embed.add_field(
-                    name=cog, value=f"`{self.bot.command_prefix}help {cog.lower()}`"
+                    name=cog_name,
+                    value=f"`{self.bot.command_prefix}help {cog_name.lower()}`",
                 )
 
-            # fields that will be shown in main help embed
             for cog_name in self.bot.cogs:
+                # fields that will be shown in main help embed
                 try:
                     for field in self.bot.get_cog(cog_name).main_help_fields:
                         help_embed.add_field(
@@ -90,60 +89,63 @@ Shows info about `ping` command:
                 except AttributeError:
                     # cog doesn't have main_help_fields
                     pass
-        else:
-            lower_cogs = [c.lower() for c in cogs]
 
-            if cog_str.lower() in lower_cogs:
-                cog = self.bot.get_cog(cogs[lower_cogs.index(cog_str.lower())])
+            await ctx.send(embed=help_embed)
+        else:  # when searching for a specific cog
+            cogs = list(self.bot.cogs.keys())
+            lower_cogs = [cog.lower() for cog in cogs]
+            lower_cog_str = cog_str.lower()  # lower case string of cog
 
-                cog_help = ""
-                try:
-                    cog_help = cog.help_msg + ("\n\n" if cog.help_msg else "")
-                except AttributeError:
-                    pass
-
-                help_embed = discord.Embed(
-                    title=f'"{cog_str.lower()}" commands',
-                    description=f"Use `{self.bot.command_prefix}help <command>` to get more information about a command. (case sensitive)\n\n"
-                    + cog_help
-                    + f"**Commands:**\n `{'`, `'.join([_.name for _ in cog.get_commands()])}`",
-                )
-            else:
-                comm: discord.ext.commands.Command = discord.utils.get(
-                    self.bot.commands, name=cog_str
-                )
-                if comm:
-                    # default usage
-                    if not comm.usage:
-                        comm.usage = "> {prefix}{command}"
-                    await ctx.send(
-                        embed=discord.Embed(
-                            title=f"{self.bot.command_prefix}{comm.name}",
-                            description=(
-                                f"**Aliases:** `{'`, `'.join(comm.aliases)}`"
-                                if comm.aliases
-                                else ""
-                            )
-                            + "\n\n**Description:**\n"
-                            + (comm.help if comm.help else "Under construction...")
-                            + "\n\n**Usage:**\n"
-                            + comm.usage.format(
-                                prefix=self.bot.command_prefix, command=comm.name
-                            )
-                            if comm.usage
-                            else "Under construction...",
-                        )
-                    )
-                    return
+            if lower_cog_str in lower_cogs:  # when a cog has been found
+                cog = self.bot.get_cog(cogs[lower_cogs.index(lower_cog_str)])
 
                 await ctx.send(
                     embed=discord.Embed(
-                        description=f"Cannot find cog/command **{cog_str}**.\nUse `{self.bot.command_prefix}help` command to list all cogs."
+                        title=f'"{lower_cog_str}" commands',
+                        description=f"Use `{self.bot.command_prefix}help <command>` to get more information about a command. (case insensitive)\n\n"
+                        # keep cog_help empty if cog.help_msg does not exist
+                        + (cog.help_msg + ("\n\n" if cog.help_msg else ""))
+                        if hasattr(cog, "help_msg")
+                        else ""
+                        + f"**Commands:**\n `{'`, `'.join([command.name for command in cog.get_commands()])}`",
                     )
                 )
-                return
+            else:  # when a cog has not been found
+                command: discord.ext.commands.Command = discord.utils.get(
+                    self.bot.commands, name=cog_str
+                )
 
-        await ctx.send(embed=help_embed)
+                if not command:  # when command was not found
+                    await ctx.send(
+                        embed=discord.Embed(
+                            description=f"Cannot find cog/command **{cog_str}**.\nUse the `{self.bot.command_prefix}help` command to list all enabled cogs."
+                        )
+                    )
+                else:  # when a command has been found
+                    await ctx.send(
+                        embed=discord.Embed(
+                            title=f"{self.bot.command_prefix}{command.name}",
+                            description=(
+                                f"**Aliases:** `{'`, `'.join(command.aliases)}`"
+                                if command.aliases
+                                else ""
+                            )
+                            + "\n\n**Description:**\n"
+                            + (
+                                command.help
+                                if command.help
+                                else "Under construction..."
+                            )
+                            + "\n\n**Usage:**\n"
+                            + (
+                                command.usage
+                                if command.usage
+                                else "> {prefix}{command}"
+                            ).format(
+                                prefix=self.bot.command_prefix, command=command.name
+                            ),
+                        )
+                    )
 
 
 def setup(bot):
