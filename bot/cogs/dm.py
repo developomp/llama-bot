ce4534bb7796560fbef76150a365a83d6131ea5d
developomp <developomp@gmail.com>
2021-06-20 16:27:50 +0900

    fixed double message on DM command

--- bot/cogs/dm.py
+++ bot/cogs/dm.py
@@ -14,15 +14,6 @@ class DM(commands.Cog):
         for emoji_raw in self.bot.VARS["dm"]["emojis"]:
             self.emojis.add(util.convert_to_partial_emoji(emoji_raw, self.bot))
 
-    async def cog_check(self, ctx: commands.Context):
-        if exception_or_bool := await util.on_pm(ctx.message, self.bot):
-            raise exception_or_bool
-        return not exception_or_bool
-
-    @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        await util.on_pm(message, self.bot)
-
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         if payload.guild_id or (payload.emoji not in self.emojis):
