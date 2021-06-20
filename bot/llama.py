cc0880dddfbfa7ea1bcbb5c96ecade9d4b150b70
developomp <developomp@gmail.com>
2021-06-20 11:03:29 +0900

    show more information in error embed and don't make unnecessary firestore call when on_ready is called multiple times

--- bot/llama.py
+++ bot/llama.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 
 from os import listdir, path
 from time import time
+from typing import Union
 import traceback
 import json
 
@@ -18,11 +19,13 @@ def resolve_path(relative_path: str):
 
 
 class Llama(commands.Bot):
+    # the server the bot is intended to run on. It's useful to have this reference available.
     LP_SERVER: discord.Guild
-    server_id = 457373827073048604
+    server_id: int = 457373827073048604
 
     # IDs of users who can run owners only commands
-    owner_ids: set = {501277805540147220, 396333737148678165}
+    owner_id: int = 501277805540147220
+    owner_ids: set = {owner_id, 396333737148678165}
 
     def __init__(self, firebase_cred_path: str, prefix: str = "-"):
         super().__init__(
@@ -31,30 +34,43 @@ class Llama(commands.Bot):
             case_insensitive=True,  # allow mix of lower cae and upper case for commands
         )
 
-        # my own firestore interface
+        # create firestore interface
         self.llama_firebase: LlamaFirebase = LlamaFirebase(firebase_cred_path)
 
-        # read all variables in the beginning to save time later
+        # read all configs and data for better responsiveness
         self.VARS = self.llama_firebase.read_collection("vars")
 
     # ----- [ DISCORD.PY STUFF ] -----
 
     async def on_ready(self):
-        """https://discordpy.readthedocs.io/en/latest/api.html#discord.on_ready"""
+        """
+        variables in this function are checked if they exist before getting defined (reading from firebase which takes time)
+        because the function on_ready() can be called multiple times as mentined in the discord.py documentaton:
+        https://discordpy.readthedocs.io/en/latest/api.html#discord.on_ready
+        """
 
         # Prevents bot from running in server other than LP's
-        self.LP_SERVER: discord.Guild = next(
-            (guild for guild in self.guilds if guild.id == self.server_id), None
-        )
+        if not hasattr(self, "LP_SERVER"):
+            self.LP_SERVER: discord.Guild = next(
+                (guild for guild in self.guilds if guild.id == self.server_id), None
+            )
+
         if not self.LP_SERVER:
             print("----------[ The bot is not in LP server! ]----------")
-            exit(-6969)
+            exit(69)
 
         # load roles
-        self.HIGHEST_ORDER = self.get_role_from_vars("HIGHEST_ORDER")
-        self.LPWB_MEMBER = self.get_role_from_vars("LPWB_MEMBER")
-        self.SILK_PERMISSION = self.get_role_from_vars("SILK_PERMISSION")
-        self.PYJAMAS = self.get_role_from_vars("PYJAMAS")
+        if not hasattr(self, "self.HIGHEST_ORDER"):
+            self.HIGHEST_ORDER = self.get_role_from_vars("HIGHEST_ORDER")
+
+        if not hasattr(self, "self.LPWB_MEMBER"):
+            self.LPWB_MEMBER = self.get_role_from_vars("LPWB_MEMBER")
+
+        if not hasattr(self, "self.SILK_PERMISSION"):
+            self.SILK_PERMISSION = self.get_role_from_vars("SILK_PERMISSION")
+
+        if not hasattr(self, "self.PYJAMAS"):
+            self.PYJAMAS = self.get_role_from_vars("PYJAMAS")
 
         # define roles with access to special features
         self.LLAMA_PERMS = [
@@ -65,7 +81,7 @@ class Llama(commands.Bot):
         ]
 
         # load cogs at the very last moment as some of them require data from the database
-        # load all cogs that does not begin with a underscore
+        # load all cogs that do not begin with a underscore
         cogs_dir = resolve_path("./cogs")
         for cog in [
             f"cogs.{path.splitext(f)[0]}"
@@ -73,98 +89,79 @@ class Llama(commands.Bot):
             if path.isfile(path.join(cogs_dir, f)) and not f[0] == "_"
         ]:
             print(f"loading cog: {cog}")
-            self.load_extension(cog)
+            try:
+                self.load_extension(cog)
+            except discord.ExtensionAlreadyLoaded:
+                print(f"Extension {cog} was already loaded. Skipping.")
 
         # to show bot uptime
-        self.start_time = time()
+        if not hasattr(self, "start_time"):
+            self.start_time = time()
+
         print(f"{self.user} is up and ready!")
 
     async def on_command_error(
         self,
-        ctx: discord.ext.commands.Context,
-        error: discord.ext.commands.CommandError,
+        ctx: commands.Context,
+        error: commands.CommandError,
     ):
-        """Gets executed when the bot encounters an error."""
+        """
+        Gets executed when the bot encounters an error.
+        """
 
-        error_message = str(error)
+        err_title: str = ""
+        err_description: str = f"Command: {ctx.message.content.split()[0]}"
 
-        # When command that is only meant to be called in admin channels are called elsewhere
         if isinstance(error, util.NotAdminChannel):
-            await ctx.send(
-                embed=discord.Embed(
-                    title="<:hinkies:766672386132934666> Not in admin channel!",
-                    description=error_message,
-                )
-            )
+            err_title = ":lock: Not in admin channel"
+            err_description = f"This command can only be called in admins channels."
 
-        # When NSFW commands are called in non NSFW channels
-        if isinstance(error, discord.ext.commands.errors.NSFWChannelRequired):
-            await ctx.send(
-                embed=discord.Embed(
-                    title=":lock: This command is not available in non NSFW channel"
-                )
-            )
+        if isinstance(error, commands.errors.NSFWChannelRequired):
+            err_title = ":lock: Not in nsfw channel"
+            err_description = "This command can only be called in NSFW channels."
 
-        # When the bot doesn't have permissions it requires to run a command
-        if isinstance(error, discord.ext.commands.errors.BotMissingPermissions):
-            missing_perms_list = "".join([f"- {i}\n" for i in error.missing_perms])
-            await ctx.send(
-                embed=discord.Embed(
-                    title="Aw man",
-                    description=f"The bot require following permissions to run command `{ctx.message.content}`.\n{missing_perms_list}",
-                )
-            )
+        if isinstance(error, commands.errors.NotOwner):
+            err_title = f":lock: Not a bot owner"
+            err_description = "Only the bot owner(s) can call that command."
 
-        # When a command that can only be called by the owners are called
-        if isinstance(error, discord.ext.commands.errors.NotOwner):
-            await ctx.send(
-                embed=discord.Embed(
-                    title="Oops!",
-                    description=f"You have to be a bot owner to run command `{ctx.message.content}`.",
-                )
-            )
+        if isinstance(error, commands.errors.BotMissingPermissions):
+            missing_perms = "".join([f"- {i}\n" for i in error.missing_perms])
+            err_title = ":exclamation: Missing Permission(s)"
+            err_description = f"missing the following permissions:\n{missing_perms}"
 
-        # When argument(s) required by the command is not passed
-        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
-            await ctx.send(
-                embed=discord.Embed(
-                    title="Error!",
-                    description=f"Command `{ctx.message.content}` is missing required argument(s).\nConsider using `{self.command_prefix}help {ctx.command}` to learn how to use it.",
-                )
-            )
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            err_title = f":exclamation: Missing required argument(s)"
+            err_description = f"Consider using the `{self.command_prefix}help {ctx.command}` command to learn how to use it."
 
-        # When invalid argument is passed
         if isinstance(
             error,
             (
-                discord.ext.commands.errors.BadArgument,
-                discord.ext.commands.ArgumentParsingError,
+                commands.errors.BadArgument,
+                commands.ArgumentParsingError,
             ),
         ):
-            await ctx.send(
-                embed=discord.Embed(
-                    title="Hol up!",
-                    description=f"command `{ctx.message.content}` is given invalid argument(s).\nConsider using `{self.command_prefix}help {ctx.command}` to learn how to use it.",
-                )
-            )
+            err_title = f":exclamation: Invalid argument(s)"
+            err_description = f"`Consider using the `{self.command_prefix}help {ctx.command}` command to learn how to use it."
 
-        # When user id doesn't correspond to anyone in the server
-        if isinstance(error, discord.ext.commands.errors.MemberNotFound):
-            await ctx.send(
-                embed=discord.Embed(
-                    title="Hmm...",
-                    description=f"Member {error.argument} was not found in this server.",
-                )
-            )
+        if isinstance(error, commands.errors.MemberNotFound):
+            err_title = f":exclamation: Member not found"
+            err_description = f"Member {error.argument} was not found in this server."
 
         # When command failed to complete
-        if isinstance(error, discord.ext.commands.errors.CommandInvokeError):
-            await ctx.send(
-                embed=discord.Embed(
-                    title="Error!",
-                    description="Command Failed to complete. This is most likely a problem on the bot's side.",
-                )
+        if isinstance(error, commands.errors.CommandInvokeError):
+            err_title = ":exclamation: Command Failed to complete"
+            err_description = "Encountered an unknown error."
+
+        await ctx.send(
+            embed=discord.Embed(
+                title=err_title,
+                description=err_description,
             )
+            .add_field(name="Channel", value=ctx.message.channel.mention)
+            .add_field(name="Author", value=ctx.author.mention)
+            .add_field(name="Message", value=f"[Message URL]({ctx.message.jump_url})")
+            .add_field(name="Content", value=ctx.message.content[:1024], inline=False)
+        )
 
         # Log details in terminal
         print("")
@@ -172,7 +169,6 @@ class Llama(commands.Bot):
         print(type(error))
         print("Cog:", ctx.cog)
         print("Author:", ctx.author, ctx.author.id)
-        # show message that actually caused the error
         print("Content:", ctx.message.content)
         print("Channel:", ctx.message.channel, ctx.message.channel.id)
         print("URL:", ctx.message.jump_url)
@@ -183,33 +179,36 @@ class Llama(commands.Bot):
 
     # ----- [ BOT METHODS ] -----
 
-    def get_role_from_vars(self, name):
-        """Get discord role by name"""
+    def get_role_from_vars(self, name) -> Union[discord.Role, None]:
+        """
+        Get discord role by name
+        """
         return discord.utils.get(self.LP_SERVER.roles, id=int(self.VARS["roles"][name]))
 
     def get_channel_from_vars(self, name) -> discord.abc.GuildChannel:
-        """Get discord channel by name"""
+        """
+        Get discord channel by name
+        """
         return self.LP_SERVER.get_channel(int(self.VARS["channels"][name]))
 
 
 def main():
-    # ignore if config.json does not exist
     try:
         with open(resolve_path("./config.json"), "rt") as f:
             config = json.loads(f.read())
     except FileNotFoundError:
+        # ignore if config.json does not exist
         pass
 
     with open(resolve_path("./secrets/secret.json"), "rt") as f:
         secret = json.loads(f.read())
 
-    default_prefix = "-"
     llama_bot = Llama(
         resolve_path("./secrets/firebase-adminsdk.json"),
-        # set default prefix
-        (config["prefix"] if "prefix" in config else default_prefix)
+        # set default prefix if it exists
+        (config["prefix"] if "prefix" in config else None)
         if "config" in locals()
-        else default_prefix,
+        else None,
     )
 
     llama_bot.run(secret["token"])
